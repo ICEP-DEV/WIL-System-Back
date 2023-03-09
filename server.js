272fe0de71cb41b09447e62fe850c1b3d46315b2
unknown <kgomotsomakoti04@gmail.com>
2023-03-09 15:55:33 +0200

    server js

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const conn = require('./config/db')
+//const studentRoute = require('./routes/students')
+const app = express();app.use(express.json());
+app.use(express.urlencoded({extend: false}));
+
+
+
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log("Db is connected successfully: ", conn.threadId);
+
+})
+
+
+
+//app.use('/api', studentRoute)
+
+
+
+app.listen(5000, ()=> console.log("server is running on port: " +8080));
