63de5c8d5785a9beb89ebec044b3127233241bb2
silenceM-cpu <silence.tshifhinga.18@gmail.com>
2023-04-25 18:00:47 +0200

    email notification

--- routes/wilForms.js
+++ routes/wilForms.js
@@ -45,4 +45,25 @@ router.post('/office', (req, res, next) => {
         res.status(200).json({'Message' :"Success"});
 });
 
+router.post('/admForms', (req,res,next)=>{
+   
+    const values = [
+     req.body.semester,
+     req.body.finacialAid,
+     req.body.campus,
+     req.body.appYear,
+
+    ]
+
+    let sql = `INSERT INTO placement (semester,finacialAid, 
+        campus, appYear) VALUES (?,?,?,?)`
+
+    conn.query(sql, values, function(err, result){
+        if(err)throw err;
+            console.log("Successfully inserted")
+    })
+
+    res.status(200).json({'Message' :"Success"});
+})
+
 module.exports = router;
