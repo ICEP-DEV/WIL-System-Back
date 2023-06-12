50c4cc620560107049a3e85fa36882cb2cc30880
unknown <silence.tshifhinga.18@gmail.com>
2023-06-12 15:48:49 +0200

    invite

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(express.urlencoded({extend: false}));
-const studentRoute = require('./routes/admissionForm')
+const studentRoute = require('./routes/sendEmail')
 
 
 conn.connect((err)=>{
