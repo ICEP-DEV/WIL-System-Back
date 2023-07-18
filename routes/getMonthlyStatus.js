270f6c31cd18ce4b080c20c1e0829a4ab14cca44
unknown <silence.tshifhinga.18@gmail.com>
2023-07-18 13:17:49 +0200

    regQuuery

--- routes/getMonthlyStatus.js
+++ routes/getMonthlyStatus.js
@@ -7,7 +7,7 @@ let studNum;
 router.get('/getMonthlyStatus/:student_no', (req, res, next) => {
     studNum = req.params.student_no;
 
-    let sql = `SELECT month, status, approval FROM monthly_status WHERE student_no = ?`;
+    let sql = `SELECT month, status, approval FROM logbook WHERE student_no = ?`;
     conn.query(sql, [studNum], (err, result) => {
         if (err) throw err;
         res.status(200).json({ result });
