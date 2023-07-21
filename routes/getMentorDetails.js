b7cb4a70a4ecf7e705605d10f0150126632202ce
unknown <kgomotsomakoti04@gmail.com>
2023-07-21 16:04:13 +0200

    getMentorInfo

--- routes/getMentorDetails.js
+++ routes/getMentorDetails.js
@@ -5,7 +5,8 @@ const router = express.Router();
 
 router.get('/getMentorInfo/:student_no', (req, res, next) => {
   const studNum = req.params.student_no;
-  let sql = `SELECT student_no, title, m_name, m_surname, mobileNo, email_address FROM mentor WHERE student_no = ?`;
+  let sql = `SELECT S.student_no, M.title, M.m_name, M.m_surname, M.mobileNo, M.email_address FROM mentor M, student S 
+  WHERE S.student_no = M.student_no AND S.student_no = ?`;
   conn.query(sql, [studNum], (err, result) => {
     if (err) throw err;
     res.status(200).json({ result });
